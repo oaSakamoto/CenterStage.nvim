d87a965e3e6c4e6e9a9e217fae9c88930374f966
Olimpio Almeida Sakamoto <oasakamoto@proton.me>
2024-10-28 16:46:12 -0300

    fix: revert wrong incrementation

--- lua/CenterStage/utils.lua
+++ lua/CenterStage/utils.lua
@@ -36,8 +36,7 @@ local function center_cursor()
     local buffer_line_count = vim.api.nvim_buf_line_count(bufnr)
     local middle_line = math.floor(window_height / 2)
 
-    -- clear_phantom_lines(bufnr)
-
+    clear_phantom_lines(bufnr)
 
     if buffer_line_count - cursor_line < middle_line then
         vim.opt_local.scrolloff = 0
